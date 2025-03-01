7aa12bddcf20f667cc7856c2b3783d387492b51d
clientesweb <alexisifran32@gmail.com>
2025-03-01 00:02:17 -0300

    Actualizar Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,22 +1,15 @@
-import type React from "react"
-import { Box, Flex, Button, Stack, IconButton, useDisclosure } from "@chakra-ui/react"
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons"
-import { FaHome, FaPlay, FaYoutube, FaInfoCircle } from "react-icons/fa"
-import Image from "next/image"
+import { Box, Flex, useDisclosure } from "@chakra-ui/react"
+import { FaHome, FaCalendarAlt, FaYoutube, FaAd } from "react-icons/fa"
 import Link from "next/link"
 
-const NavLink = ({ children, href }: { children: React.ReactNode; href: string }) => (
+const NavItem = ({ icon, href, label }) => (
   <Link href={href} passHref>
-    <Button
-      as="a"
-      variant="ghost"
-      color="white"
-      _hover={{
-        bg: "whiteAlpha.200",
-      }}
-    >
-      {children}
-    </Button>
+    <Flex as="a" align="center" flexDir="column" py={2} px={4} color="whiteAlpha.700" _hover={{ color: "white" }}>
+      {icon}
+      <Box fontSize="xs" mt={1}>
+        {label}
+      </Box>
+    </Flex>
   </Link>
 )
 
@@ -24,56 +17,13 @@ export const Navigation = () => {
   const { isOpen, onToggle } = useDisclosure()
 
   return (
-    <Box position="fixed" w="100%" zIndex={1000} bg="brand.500">
-      <Flex minH={"60px"} py={{ base: 2 }} px={{ base: 4 }} align={"center"} justify={"space-between"}>
-        <Flex align="center">
-          <Image src="/android-chrome-192x192.png" alt="Café Club TV Logo" width={40} height={40} />
-        </Flex>
-
-        <Flex display={{ base: "none", md: "flex" }}>
-          <Stack direction={"row"} spacing={4}>
-            <NavLink href="/">
-              <FaHome /> Inicio
-            </NavLink>
-            <NavLink href="/programacion">
-              <FaPlay /> Programación
-            </NavLink>
-            <NavLink href="/shorts">
-              <FaYoutube /> Shorts
-            </NavLink>
-            <NavLink href="/nosotros">
-              <FaInfoCircle /> Nosotros
-            </NavLink>
-          </Stack>
-        </Flex>
-
-        <IconButton
-          display={{ base: "flex", md: "none" }}
-          onClick={onToggle}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          variant={"ghost"}
-          color="white"
-          aria-label={"Toggle Navigation"}
-        />
+    <Box position="fixed" bottom={0} left={0} right={0} zIndex={1000}>
+      <Flex bg="gray.900" color="white" align="center" justify="space-around" wrap="wrap" py={2} px={4}>
+        <NavItem icon={<FaHome size={24} />} href="/" label="Inicio" />
+        <NavItem icon={<FaCalendarAlt size={24} />} href="/parrilla" label="Parrilla" />
+        <NavItem icon={<FaYoutube size={24} />} href="/shorts" label="Shorts" />
+        <NavItem icon={<FaAd size={24} />} href="/sponsors" label="Sponsors" />
       </Flex>
-
-      {/* Mobile menu */}
-      <Box display={{ base: isOpen ? "block" : "none", md: "none" }} bg="brand.500" p={4}>
-        <Stack spacing={4}>
-          <NavLink href="/">
-            <FaHome /> Inicio
-          </NavLink>
-          <NavLink href="/programacion">
-            <FaPlay /> Programación
-          </NavLink>
-          <NavLink href="/shorts">
-            <FaYoutube /> Shorts
-          </NavLink>
-          <NavLink href="/nosotros">
-            <FaInfoCircle /> Nosotros
-          </NavLink>
-        </Stack>
-      </Box>
     </Box>
   )
 }
