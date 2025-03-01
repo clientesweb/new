dd495272a9f14fad49d227211ce3ae0604241726
clientesweb <alexisifran32@gmail.com>
2025-03-01 00:08:00 -0300

    Actualizar _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,53 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import { ColorModeScript } from '@chakra-ui/react';
+import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+import { ColorModeScript } from "@chakra-ui/react"
 
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="es">
         <Head>
-          <link rel='manifest' href='/manifest.json' />
-          <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-          <meta name='theme-color' content='#fff' />
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#C41E3A" />
+          <meta
+            name="description"
+            content="Tu destino de entretenimiento con la mejor programación, shorts y contenido exclusivo."
+          />
+
+          {/* PWA primary color */}
+          <meta name="theme-color" content="#C41E3A" />
+
+          {/* Favicon */}
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+          {/* Apple Touch Icon */}
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+          {/* Android Chrome Icons */}
+          <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+          <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+
+          {/* Web App Manifest */}
+          <link rel="manifest" href="/manifest.json" />
+
+          {/* iOS meta tags */}
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-title" content="Café Club TV" />
+
+          {/* Windows meta tags */}
+          <meta name="msapplication-TileColor" content="#C41E3A" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
         </Head>
         <body>
-          {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
+
