3abd4ea2ebdfc8817f08accb7e17ca70ec8add39
clientesweb <alexisifran32@gmail.com>
2025-02-28 23:48:42 -0300

    Actualizar Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,18 +1,19 @@
-import { Flex, FlexProps } from '@chakra-ui/react';
+import { Flex, type FlexProps } from "@chakra-ui/react"
 
 export const Container = (props: FlexProps) => (
   <Flex
-    id='container'
-    direction='column'
-    alignItems='center'
-    justifyContent='flex-start'
-    bg='#e4e0e1'
-    color='black'
+    direction="column"
+    alignItems="center"
+    justifyContent="flex-start"
+    bg="white"
+    color="black"
     _dark={{
-      bg: '#181C1D',
-      color: 'white',
+      bg: "#121212",
+      color: "white",
     }}
-    transition='all 0.15s ease-out'
+    transition="all 0.15s ease-out"
+    minH="100vh"
     {...props}
   />
-);
+)
+
